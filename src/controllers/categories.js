fb40ce300d3ef11ad2bc94a8733d4b05815422ea
tsbowers <trevorbowers3@gmail.com>
2026-07-18 19:37:30 -0500

    help

--- src/controllers/categories.js
+++ src/controllers/categories.js
@@ -1,17 +1,19 @@
 import { getAllCategories, getCategoryById, getProjectsByCategoryId } from '../models/categories.js';
 
-const showCategoriesPage = async (req, res) => {
+export const showCategoriesPage = async (req, res) => {
     try {
         const categories = await getAllCategories();
-        const title = 'Service Categories';
-        res.render('categories', { title, categories });
+        res.render('categories', { 
+            title: 'Service Project Categories', 
+            categories 
+        });
     } catch (error) {
         console.error('Error fetching categories:', error);
         res.status(500).send('Internal Server Error');
     }
-};  
+};
 
-const showCategoryDetailsPage = async (req, res) => {
+export const showCategoryDetailsPage = async (req, res) => {
     try {
         const categoryId = req.params.id;
         const category = await getCategoryById(categoryId);
@@ -19,15 +21,16 @@ const showCategoryDetailsPage = async (req, res) => {
         if (!category) {
             return res.status(404).render('errors/404', { title: 'Category Not Found' });
         }
-
+        
         const projects = await getProjectsByCategoryId(categoryId);
-        const title = category.name;
-
-        res.render('category', { title, category, projects });
+        
+        res.render('category', {
+            title: category.name,
+            category,
+            projects
+        });
     } catch (error) {
         console.error('Error fetching category details:', error);
         res.status(500).send('Internal Server Error');
     }
 };
-
-export { showCategoriesPage, showCategoryDetailsPage };
